c12d3cd875a7e6afa18e02763de99848e86b2d62
Tomas Hardy <59655210+HardyTomas@users.noreply.github.com>
2020-04-01 03:53:31 +0000

    Up

--- listgetter.py
+++ listgetter.py
@@ -12,4 +12,6 @@ sl = str(input("SSL Mode ( yes / no / all ) : "))
 file = str(input("File Name (EX : proxies.txt / socks4.txt / socks5.txt ) : "))
 rsp = requests.get('https://api.proxyscrape.com/?request=displayproxies&proxytype='+str(type)+'&country='+str(cty)+'&anonymity='+str(anon)+'&ssl='+str(sl)+'&timeout='+str(to))
 with open(str(file),'wb') as fp:
+rsp = requests.get('https://free-proxy.net?request=displayproxies&proxytype='+str(type)+'&country='+str(cty)+'&anonymity='+str(anon)+'&ssl='+str(sl)+'&timeout='+str(to))
+with open(str(file),'wb') as fp:
 	fp.write(rsp.content)
